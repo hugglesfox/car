0835ad8873039e7ffb6d123c9d3ef0bb1ade1aac
Hayley Hughes <hayley@foxes.systems>
2023-05-07 18:33:15 +1000

    Reorganise repo

--- install.sh
+++ install.sh
@@ -18,25 +18,23 @@ rfkill unblock all
 cp config.txt /boot/config.txt
 cp cmdline.txt /boot/cmdline.txt
 
-cp gps.conf /etc/modules-load.d/gps.conf
-cp 50-gps.rules /etc/udev/rules.d/50-gps.rules
-cp gpsd /etc/default/gpsd
-cp chrony.conf /etc/chrony/conf.d/car.conf
+cp etc/chrony.conf /etc/chrony/conf.d/car.conf
+cp etc/dhcpcd.conf /etc/dhcpcd.conf
+cp etc/dnsmasq.conf /etc/dnsmasq.d/car.conf
+cp etc/hostapd.conf /etc/hostapd/hostapd.conf
 
-cp hostapd.conf /etc/hostapd/hostapd.conf
-cp dnsmasq.conf /etc/dnsmasq.d/car.conf
-cp dhcpcd.conf /etc/dhcpcd.conf
-
-cp rfcomm.service /etc/systemd/system/rfcomm.service
-cp rfcomm /etc/defaults/rfcomm
+cp defaults/* /etc/defaults/
+cp modules-load.d/* /etc/modules-load.d/
+cp systemd/* /etc/systemd/system/
+cp udev/* /etc/udev/rules.d/
 
 systemctl daemon-reload
 
 systemctl unmask hostapd
 
+systemctl enable docker
 systemctl enable hostapd
 systemctl enable rfcomm
-systemctl enable docker
 
 echo "192.168.0.1    car" >> /etc/hosts
 
